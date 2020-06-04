047d473ab8d64791b5bef262389b438d2864c36f
Francisco Roldan <fr673@nyu.edu>
2020-06-04 10:29:06 -0400

    Reports on how to run at beginning

--- Codes/main_privres.jl
+++ Codes/main_privres.jl
@@ -1,3 +1,4 @@
+println("Reserve Accumulation with a Private IIP")
 using QuantEcon, Interpolations, Optim, Dates, Printf, Random, JLD, PlotlyJS, ColorSchemes, ORCA, JSON
 
 include("type_def.jl")
@@ -5,3 +6,6 @@ include("handle_itps.jl")
 include("debtprice.jl")
 include("planner.jl")
 include("reporting_routines.jl")
+
+println("Constructor sr = SOEres()")
+println("Main loop vfi!(sr; tol::Float64=1e-4, maxiter::Int64=500, verbose::Bool=false)")
